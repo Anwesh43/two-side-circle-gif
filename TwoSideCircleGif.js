0e8d1915f947b70dfb104d26222392ca6c6998d3
Anwesh43 <anweshthecool0@gmail.com>
2018-10-06 18:52:08 +0530

    adding fixes for animation

--- TwoSideCircleGif.js
+++ TwoSideCircleGif.js
@@ -12,6 +12,7 @@ class State {
 
     update(cb) {
         this.scale += 0.05 * this.dir
+        console.log(`scale ${this.scale}`)
         if (Math.abs(this.scale - this.prevScale) > 1) {
             this.scale = this.prevScale + this.dir
             this.dir = 0
@@ -20,10 +21,9 @@ class State {
         }
     }
 
-    startUpdating(cb) {
+    startUpdating() {
         if (this.dir == 0) {
             this.dir = 1 - 2 * this.prevScale
-            cb()
         }
     }
 }
@@ -50,9 +50,11 @@ class TSCNode {
         context.strokeStyle = '#1565C0'
         context.save()
         context.translate(this.i * gap + gap, h/2)
-        for (var j = 0; j < 2; j++) {
-            const sf = 1 - 2 * j
-            const sc = Math.min(0.5, this.state.scale - 0.5 * j)
+        for (var i = 0; i < 2; i++) {
+            const sf = 1 - 2 * i
+            const sc = Math.min(0.5, Math.max(this.state.scale - 0.5 * i, 0)) * 2
+            context.save()
+            context.scale(sf, 1)
             context.beginPath()
             var k = 0
             var deg = 90 * sc
@@ -66,6 +68,7 @@ class TSCNode {
                 k++
             }
             context.stroke()
+            context.restore()
         }
         context.restore()
         if (this.prev) {
@@ -121,13 +124,14 @@ class TwoSideCircle {
 
 class Renderer {
     constructor() {
-        this.running = false
+        this.running = true
         this.curr = new TwoSideCircle()
     }
 
     render(context, cb, endcb) {
-        if (this.running) {
+        while (this.running) {
             context.fillStyle = '#212121'
+            context.fillRect(0, 0, w, h)
             this.curr.draw(context)
             cb(context)
             this.curr.update(() => {
